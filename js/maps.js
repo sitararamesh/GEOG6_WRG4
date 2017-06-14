d8a8270ef4eb7474a59874f5e923c9a7c5f06d30
Sitara Ramesh <sitararamesh@hotmail.com>
2017-06-14 16:44:16 -0700

    update

--- js/maps.js
+++ js/maps.js
@@ -1,6 +1,6 @@
 function initMap() {
         var map = new google.maps.Map(document.getElementById('map'), {
-          zoom: 3,
+          zoom: 1,
           center: {lat: 0, lng: 180}
         });
         var LatLng1 = {lat: 48.9914939, lng: -123.0563069};
