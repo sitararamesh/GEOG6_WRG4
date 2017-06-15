3814c49127cec56bd8c7f830a35adae93945f5cb
Sitara Ramesh <sitararamesh@hotmail.com>
2017-06-14 19:03:41 -0700

    Central America

--- js/maps.js
+++ js/maps.js
@@ -1,8 +1,9 @@
 function initMap() {
         var map = new google.maps.Map(document.getElementById('map'), {
-          zoom: 1,
+          zoom: 2,
           center: {lat: 0, lng: 180}
         });
+        // north america
         var LatLng1 = {lat: 48.9914939, lng: -123.0563069};
         var north_america = new google.maps.Marker({
           position: LatLng1,
@@ -11,6 +12,15 @@ function initMap() {
 
         north_america.addListener('click', function() {
           window.open('./northamerica.html');
-          console.log("JUNK");
+        });
+        // central america
+        var LatLng2= {lat:10.920217, lng: -83.682587};
+        var central_america = new new google.maps.Marker({
+          position: LatLng2,
+          map: map
+        });
+        central_america.addListener('click', function() {
+          window.open('./centralamerica.html');
+          console.log("TEST");
         });
       }
