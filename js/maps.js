3ee6c8d672f83b83e75b7d8b4d11810d66c5c7e0
Sitara Ramesh <sitararamesh@hotmail.com>
2017-06-14 21:10:51 -0700

    Europe

--- js/maps.js
+++ js/maps.js
@@ -1,7 +1,7 @@
 function initMap() {
         var map = new google.maps.Map(document.getElementById('map'), {
           zoom: 2,
-          center: {lat: 0, lng: 180}
+          center: {lat: 30, lng: 0}
         });
         // north america
         var LatLng1 = {lat: 48.9914939, lng: -123.0563069};
@@ -13,14 +13,37 @@ function initMap() {
         north_america.addListener('click', function() {
           window.open('./northamerica.html');
         });
+
         // central america
-        var LatLng2= {lat:10.920217, lng: -83.682587};
-        var central_america = new new google.maps.Marker({
+        var LatLng2 = {lat: 10.920217, lng: -83.682587};
+        var central_america = new google.maps.Marker({
           position: LatLng2,
           map: map
         });
         central_america.addListener('click', function() {
           window.open('./centralamerica.html');
-          console.log("TEST");
+        });
+
+        // Chile and Argentina
+        //var LatLng3= {l}
+
+        //Europe
+        var LatLng4= {lat:51.442435, lng:4.927775};
+        var europe = new google.maps.Marker({
+          position: LatLng4,
+          map: map
+        });
+        europe.addListener('click', function() {
+          window.open('./europe.html');
+        });
+
+        //Middle East
+        var LatLng6= {lat:25.267471, lng:56.279532};
+        var middle_east = new google.maps.Marker({
+          position: LatLng6,
+          map: map
+        });
+        middle_east.addListener('click', function() {
+          window.open('./middleeast.html');
         });
       }
